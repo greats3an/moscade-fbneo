5dd530588451d14d970d5f4d1cd75a011a9affa6
greats3an <greats3an@gmail.com>
2021-10-30 10:29:32 +0800

    nexus : optimize io routine

--- src/dep/python/nexus.py
+++ src/dep/python/nexus.py
@@ -11,12 +11,12 @@ from threading import Lock, Thread
 from time import sleep, time_ns
 import logging,socket,sys,traceback
 
-import websocket
+import websocket,os
 # pip install websocket-client
 
 class UDPServer(Thread):
     def __init__(self,listen_address , listen_port , ws_uri):
-        self.shutdown = False
+        self.shutdown_ = False
 
         self.server_address = (listen_address,int(listen_port))
         self.ws_uri = ws_uri
@@ -24,66 +24,35 @@ class UDPServer(Thread):
         self.fd = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         self.fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.fd.bind(self.server_address)
-        self.logger = logging.getLogger('UDPServer')
-        self.idents = dict()
-        self.uuid = pack('<L',time_ns() & 0xFFFFFFFF)
+        self.logger = logging.getLogger('UDPServer')        
+        
+        self.conns = dict()
 
-        self.ws_lock = Lock()
-        with self.ws_lock:
-            self.ws = websocket.WebSocket()
-            self.ws.connect(self.ws_uri)
+        self.ws = websocket.WebSocket()
+        def ws_run():
+            self.ws.connect(self.ws_uri)            
+            print('** READY : udp/%s:%s <-> %s' % (*self.server_address,self.ws_uri))
+            while not self.shutdown_:
+                self.on_ws_recv(self.ws.recv())
+        self.ws_thread = Thread(target=ws_run)
+        self.ws_thread.daemon = True
+        self.ws_thread.start()
 
-        self.recv_thread = Thread(target=self.recv_ws,daemon=True)        
-        self.recv_lock = Lock()        
-        self.recv_thread.start()        
-                
         super().__init__(daemon=True)
-        print('** READY : udp/%s:%s <-> %s' % (*self.server_address,self.ws_uri))
-
-    def recv_ws(self):
-        while not self.shutdown:    
-            try:    
-                buf = self.ws.recv()
-                assert type(buf) != str
-                # self.logger.error('WSOCK %s' % buf.hex())
-                ident, data = buf[:10],buf[10:]
-                with self.recv_lock:
-                    for ident_ in self.idents:
-                        if ident_ != ident:
-                            self.fd.sendto(data,self.ident2addr(ident_))
-                            # self.logger.error('WSOCK %s -> %s' % (baddr,addr))     
-            except Exception as e:
-                self.logger.fatal(traceback.format_exc())
-                self.logger.fatal("CONNECTION LOST : %s" % e)
-                suicide()                
-
-    def addr2bytes(self, addr : tuple):	        	
-        '''packing address tuple, 6bytes'''
-        host,port = addr
-        return socket.inet_aton(host) + pack('H', port)
-
-    def bytes2addr(self, b : bytes):
-        '''unpacking address tuple, 6bytes'''
-        host,port = b[:4],b[4:]
-        return socket.inet_ntoa(host), unpack('H',port)[0]    
-
-    def ident2addr(self,ident):
-        return self.bytes2addr(ident[:6])
-
-    def create_ident(self,addr):
-        '''creates 10-byte network ident,unqiue to each ws connection'''
-        return self.addr2bytes(addr) + self.uuid
+        
+    def on_ws_recv(self,buf):        
+        port,data = unpack('H',(buf[:2]))[0],buf[2:]
+        for port_ in self.conns:
+            if port != port_:                            
+                self.fd.sendto(data,('127.0.0.1',port_))
 
     def run(self):                
-        while not self.shutdown:                     
-            # new connection w/ data to be sent                
-            data, addr = self.fd.recvfrom(4096)
-            # self.logger.error('UDP %s:%s -> %s' % (*addr,data.hex()))
-            ident = self.create_ident(addr)
-            with self.ws_lock:
-                self.ws.send_binary(ident + data)
-            with self.recv_lock:
-                self.idents[ident] = 1 # marking the address where proceeding data will be send to
+        while not self.shutdown_:                     
+            # new connection w/ data to be sent
+            data, addr = self.fd.recvfrom(64)                                                
+            self.ws.send_binary(pack('H', addr[1]) + data)                    
+            self.conns[addr[1]] = True
+        
 
 class TCPServer(ThreadingMixIn,TCPServer):
     def __init__(self, listen_address , listen_port , ws_uri):
@@ -92,38 +61,35 @@ class TCPServer(ThreadingMixIn,TCPServer):
         print('** READY : tcp/%s:%s <-> %s' % (*self.server_address,self.ws_uri))
 
 class TCPHandler(BaseRequestHandler):
+    def __init__(self, request, client_address, server) -> None:
+        self.shutdown_ = False
+        super().__init__(request, client_address, server)        
+
     def handle(self) -> None:        
         self.server : TCPServer        
         self.ws = websocket.WebSocket()        
         self.ws.connect(self.server.ws_uri)         
-        self.request.setblocking(False)
-        self.logger = logging.getLogger('NODE (%s) %s:%s' % (self.server.socket_type.name,*self.client_address))
-        self.logger.setLevel(logging.ERROR)        
-        self.shutdown_ = False
+        self.request.setblocking(False)                
         # Maintain this connection whilst both sockets are alive
         buf = bytearray()
         while not self.shutdown_:            
             s1,s2 = self.request,self.ws.sock
             r,w,_ = select([s1,s2],[],[])
             try:
-                if (s1 in r):                
+                if s1 in r:                
                     buf_ = s1.recv(4096)                    
-                    # self.logger.error('TCP: %s' % buf_.hex())
                     self.ws.send_binary(buf_)
-                if (s2 in r):                
+                if s2 in r:                
                     buf_ = self.ws.recv()
-                    # self.logger.error('WSOCK %s' % buf_.hex())
                     buf += buf_
-                if (buf):                
+                if buf:                
                     buf = buf[s1.send(buf):]            
             except Exception as e:
-                self.logger.fatal(traceback.format_exc())
-                self.logger.fatal("Exception : %s" % e)
+                print(traceback.format_exc())                
                 self.ws.close()
                 if self.request : self.request.close()        
-                break
-            sleep(0.01)
-        self.logger.fatal("CONNECTION LOST")
+                return suicide()   
+            sleep(0.001)        
         suicide()                
 
 is_shutting_down = False
@@ -133,9 +99,9 @@ def suicide():
         return
     is_shutting_down = True
     logging.fatal('Shutting down')
-    srv_u.shutdown = True
-    srv.shutdown()
-    sys.exit(1)
+    srv_u.shutdown_ = True
+    srv.shutdown_ = True
+    os.kill(os.getpid(), 9)
 
 if __name__ == '__main__':
     argparse = ArgumentParser(description='GGPOSRV3 / MOSCade proxy')
@@ -159,5 +125,5 @@ if __name__ == '__main__':
     except Exception:
         pass
     finally:             
-        srv_u.shutdown = True
+        srv_u.shutdown_ = True
         suicide()
